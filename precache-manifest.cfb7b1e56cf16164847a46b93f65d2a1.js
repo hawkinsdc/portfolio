dbd68ee7199a49f792f58647f9e081583a64692a
Chris Hawkins <chrishawkins@Chriss-MacBook-Pro.local>
2019-07-18 01:59:47 +0100

    Updates

---
+++ precache-manifest.cfb7b1e56cf16164847a46b93f65d2a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52283b1d22b9ef68feffba10a6d6d604",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f46ad4ccb12aced16d9e",
+    "url": "/static/css/main.badee2aa.chunk.css"
+  },
+  {
+    "revision": "4c95f44f399b6150c266",
+    "url": "/static/js/2.0007278a.chunk.js"
+  },
+  {
+    "revision": "f46ad4ccb12aced16d9e",
+    "url": "/static/js/main.060f3aab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ad789581dd3bfe802dd2e6ea0c2f5caa",
+    "url": "/static/media/clouds.ad789581.svg"
+  },
+  {
+    "revision": "d235ab65ea35c3fcade47b8f5c6c4ae7",
+    "url": "/static/media/rain.d235ab65.svg"
+  },
+  {
+    "revision": "3def7c9fb95731ac622871fb6925d5f6",
+    "url": "/static/media/snow.3def7c9f.svg"
+  }
+]);
